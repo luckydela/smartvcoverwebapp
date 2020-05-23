c91405c501cba13772e145d22c83c5e15bb888d1
Allen <alleneben@gmail.com>
2020-05-23 11:38:07 -0400

    Setup pagescroutes

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -7,14 +7,75 @@ import { Router } from '@angular/router';
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
-
+  menus:any = [
+    {
+      name: 'Policies',
+      submenus: [
+        {
+          name: 'Buy Policy',
+          img: 'buy-16.png',
+          path:'buypolicy'
+        },
+        {
+          name: 'Review Policy',
+          img:'buy-2-16.png',
+          path: 'mypolicies'
+        },
+        {
+          name: 'My Policies',
+          img:'text-file-5-16.png',
+          path: 'mypolicies'
+        }
+      ]
+    },
+    {
+      name: 'Claims',
+      submenus: [
+        {
+          name: 'Make a new claim',
+          img: 'add-user-2-16.png',
+          path: 'make-a-claim'
+        },
+        {
+          name: 'My claim status',
+          img:'outline-16.png',
+          path: 'claimstatus'
+        },
+        {
+          name: 'My Claims',
+          img:'text-file-5-16.png',
+          path: 'myclaims'
+        }
+      ]
+    },
+    {
+      name: 'Other',
+      submenus: [
+        {
+          name: 'My sticker',
+          img: 'checked-user-16.png',
+          path: 'mysitcker'
+        },
+        {
+          name: 'Notification',
+          img:'check-mark-3-16.png',
+          path: 'notifications'
+        },
+        {
+          name: 'Settings',
+          img:'user-3-16.png',
+          path: 'settings'
+        }
+      ]
+    }
+  ]
   constructor(private router: Router ) { }
 
   ngOnInit() {
   }
 
-  route(){
-    this.router.navigate(['/dashboard'])
+  route(path:any){
+    this.router.navigate([path])
   }
 
 }
