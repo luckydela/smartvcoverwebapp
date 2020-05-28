456130457529e001cf7add1d347c7bdfbacdf98f
Allen <alleneben@gmail.com>
2020-05-28 04:07:57 -0400

    Forms and notifications

--- src/app/modules/motorinsurance/motorinsurance.component.ts
+++ src/app/modules/motorinsurance/motorinsurance.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { AppService } from '../../app.service';
 
@@ -13,32 +13,57 @@ import { AppService } from '../../app.service';
 })
 export class MotorinsuranceComponent implements OnInit {
   form: FormGroup
-  constructor(private router: Router, private appservice: AppService) {
+  schedules:any;
+  bodytypes:any;
+  covertypes:any=[{name:'Third Pary',code:1},{name:'Comprehensive',code:2},{name: 'Third Party Fire & Theft', code:3}];
+  makes:any;
+  years:any = this.appservice.numCombo(1951,2020,'name','code');
+  durations:any=[{name:'One Year',code:1},{name:'Short Rate',code:2},{name: 'Prorata', code:3}];;
+  notification:any = {message:'', type:'', show: false, time: ''}
+  messagetypes = {primary:'', danger:'#fff5f5'}
 
-    this.form = new FormGroup({
-      customer_first_name: new FormControl(''),
-      customer_other_names: new FormControl(''),
-      customer_last_name: new FormControl(''),
-      customer_phone: new FormControl(''),
-      customer_email: new FormControl(''),
-      cover_type_code: new FormControl(''),
-      schedule_code: new FormControl(''),
-      year_of_manufacture: new FormControl(''),
-      duration: new FormControl(''), 
-      make:new FormControl(''),
-      seats:new FormControl(''),
-      model:new FormControl(''),
-      cubic_capacity:new FormControl(''),
-      mileage:new FormControl(''),
-      vehicle_registration:new FormControl(''),
-      chasis_number: new FormControl(''),
-      body_type_code:new FormControl(''),
-      cost_of_car:new FormControl(''),
+  constructor(private router: Router, private appservice: AppService,private fb: FormBuilder) {
 
+    this.form = this.fb.group({
+      customer_first_name: [''],
+      customer_other_names: [''],
+      customer_last_name: [''],
+      customer_phone: [''],
+      customer_email: [''],
+      cover_type_code: [''],
+      schedule_code: [''],
+      year_of_manufacture: [''],
+      duration: [''], 
+      make:[''],
+      seats:[''],
+      model:[''],
+      cubic_capacity:[''],
+      mileage:[''],
+      vehicle_registration:[''],
+      chasis_number: [''],
+      body_type_code:[''],
+      cost_of_car:['']
     })
   }
 
   ngOnInit() {
+    this.appservice.getcombodata()
+    .subscribe(rd => {
+      this.schedules = rd[0];
+      this.bodytypes = rd[1];     
+    },err => console.log(err))
+    
+
+  }
+
+  onChangeYear(e:any) {
+    this.appservice.getmakecombodata(e.target.value)
+    .subscribe(rd => {
+      this.makes = rd
+    },err =>{
+      this.notification = {message:err.message, type:'danger', show: true, time: 3000}
+    })
+    
   }
   route(path:any){
     this.router.navigate([path])
---
+++ src/app/shared/components/notification/notification.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss']
+})
+export class NotificationComponent implements OnInit {
+
+  @Input() message:any = ''
+  @Input() type:any = '' 
+  @Input() show:boolean = false;
+  @Input() time:any = ''
+  constructor() { }
+
+  ngOnInit() {
+
+    
+  }
+  ngAfterViewInit(){
+    setTimeout(()=>{
+      this.show = false
+    }, this.time)
+    console.log('allen');
+    
+  }
+  ngAfterContentInit(): void {
+    console.log('Kwasi');
+    
+  }
+}
